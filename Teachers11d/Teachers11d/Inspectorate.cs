a859072196065901f2a7163fe0962291b17ab50d
DianIvanov05 <121618677+DianIvanov05@users.noreply.github.com>
2023-06-11 12:09:59 +0300

    Add files via upload

---
+++ Teachers11d/Teachers11d/Inspectorate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teachers11d
+{
+    public abstract class Inspectorate:IPrint
+    {
+        protected string inspectorateName;
+        protected int surveyNumber;
+
+        public Inspectorate(string inspectorateName, int surveryNumber)
+        {
+            this.inspectorateName = inspectorateName;
+            this.surveyNumber = surveryNumber;
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine($" {this.inspectorateName}, {this.surveyNumber}");
+        }
+
+        public abstract void  Count();
+
+    }
+}
---
+++ Teachers11d/Teachers11d/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teachers11d
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ënter the count of the teacher to be tested: ");
+            List<Inspectorate> testList = new List<Inspectorate>();
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                var data = Console.ReadLine().Split();
+                Inspectorate probe1 = new Teacher(data[0], data[1], data[2], data[3], data[4], data[5], int.Parse(data[6]));
+                testList.Add(probe1);
+            }
+            Console.WriteLine(testList.OrderBy(x=> x));
+
+
+        }
+    }
+}
